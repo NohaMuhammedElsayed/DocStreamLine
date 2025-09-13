af8d1582313c1568f7e754c2750d1b9bbe5205f9
NohaMuhammedElsayed <nohamuhammed24@gmail.com>
2025-09-13 18:12:48 +0300

    7hj

--- client/src/pages/Assistant.jsx
+++ client/src/pages/Assistant.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Assistant = () => {
   return (
-    <div className="p-8 text-2xl font-bold text-indigo-700">My Assistant 55 Page</div>
+    <div className="p-8 text-2xl font-bold text-indigo-700">My Assistant  Page</div>
   );
 };
 
